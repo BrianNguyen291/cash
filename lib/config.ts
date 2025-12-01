c5a08e52295150ccf95802d3e453cfa5e11a6cb8
BrianNguyen291 <nguyenvanqui291@gmail.com>
2025-12-01 16:03:15 +0800

    Update contact phone number in config.ts to reflect new information

--- lib/config.ts
+++ lib/config.ts
@@ -2,7 +2,7 @@
 // Update these values when you have the new contact information
 
 export const contactConfig = {
-  phone: "0933118656", // TODO: Update with new phone number
+  phone: "0912253470", // TODO: Update with new phone number
   lineUrl: "https://line.me/ti/p/7Gm6J31NKY", // TODO: Update with new LINE URL
   email: "ilove265615@yahoo.com.tw",
   companyName: "耀躍科技有限公司"
