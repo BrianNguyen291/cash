1b7fc406f9be84706c4e516db98538f8e0d7c263
BrianNguyen291 <nguyenvanqui291@gmail.com>
2026-03-07 17:57:10 +0800

    Revert "feat: Add GlobalFooter component and integrate it into the RootLayout"
    This reverts commit c97542daaa5b524442df87fe671530125b0404df.

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,6 @@ import { Header } from "@/components/header"
 import { FloatingButtons } from "@/components/floating-buttons"
 import GoogleTag from "@/components/google-tag"
 import { FraudPreventionModal } from "@/components/fraud-prevention-modal"
-import { GlobalFooter } from "@/components/global-footer"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -47,7 +46,6 @@ export default function RootLayout({
         <GoogleTag />
         <Header />
         {children}
-        <GlobalFooter />
         <FloatingButtons />
         <FraudPreventionModal />
         <script
